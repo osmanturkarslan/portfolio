bf483a101b76d7308c82378455f21a008122e250
osmanturkarslan <osmanturkarslan1620@gmail.com>
2026-03-18 09:06:38 +0100

    vite.config update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from 'url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // GitHub Pages için repository adını yazın
+  base: "/portfolio/", // GitHub Pages için repository adını yazın
+  build: {
+    outDir: "dist",
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
